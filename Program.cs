2a7a01b6e94e9c407d9782acfd130c6e1be601a3
Joshua King <joshuaking85@gmail.com>
2020-06-06 15:50:23 -0400

    First Commit of SoccerStats Application

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace SoccerStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var files = directory.GetFiles("*.txt");
+            foreach(var file in files)
+            {
+                Console.WriteLine(file.Name);
+            }
+        }
+    }
+}
