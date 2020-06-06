881ae25b3eecf529095c48a3bcf153c25c3883f1
Joshua King <joshuaking85@gmail.com>
2020-06-06 16:50:24 -0400

    Setup CSV file to read contents

--- Program.cs
+++ Program.cs
@@ -10,15 +10,16 @@ namespace SoccerStats
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
 
-            var fileName = Path.Combine(directory.FullName, "data.txt");
-            var file = new FileInfo(fileName);
-            if (file.Exists)
+            var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
+            var fileContents = ReadFile(fileName);
+            Console.WriteLine(fileContents);
+        }
+
+        public static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
             {
-                using (var reader = new StreamReader(file.FullName))
-                {
-                    Console.SetIn(reader);
-                    Console.WriteLine(Console.ReadLine());
-                }
+                return reader.ReadToEnd();
             }
         }
     }
