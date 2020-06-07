13e639ddcd44c2e5411181365e5c29e9f0be9f38
Joshua King <joshuaking85@gmail.com>
2020-06-07 13:18:30 -0400

    Added Possession Percentage to the conditional check and pushing to game results.

--- GameResults.cs
+++ GameResults.cs
@@ -14,6 +14,7 @@ namespace SoccerStats
         public int GoalAttempts { get; set; }
         public int ShotsOnGoal { get; set; }
         public int ShotsOffGoal { get; set; }
+        public double PossessionPercent { get; set; }
     }
     public enum HomeOrAway
     {
--- Program.cs
+++ Program.cs
@@ -64,6 +64,11 @@ namespace SoccerStats
                     {
                         gameResult.ShotsOffGoal = parseInt;
                     }
+                    double possessionPercent;
+                    if (double.TryParse(values[7], out possessionPercent))
+                    {
+                        gameResult.PossessionPercent = possessionPercent;
+                    }
                     soccerResults.Add(gameResult);
                 }
             }
